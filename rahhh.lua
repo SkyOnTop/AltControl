2c0808ff5bb8563a3539396a49f7c827f08c90a7
Sky <85958646+SkyOnTop@users.noreply.github.com>
2023-11-02 14:54:07 +0000

    Update rahhh.lua

--- rahhh.lua
+++ rahhh.lua
@@ -38,4 +38,4 @@ getgenv().Settings = {
 	['CustomSong'] = 6853070044 --// ! Enter A Song ID And Say 'Song!' ! \\--
 }
 --//---------------------------------------------------------------------------------------------------------------\\-- Loadstring --//
-loadstring(game:HttpGet(`https://raw.githubusercontent.com/ticxyylos/ticxyylos.github.io/main/Stand/V9.2`))()
+loadstring(game:HttpGet(`https://raw.githubusercontent.com/SkyOnTop/AltControl/main/new.lua`))()
