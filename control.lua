a78a76f716872c4957ed91b83a566e8b072364b3
Sky <85958646+SkyOnTop@users.noreply.github.com>
2024-01-03 16:57:45 +0000

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 getgenv().LynixSettings = {
     --// ! Main ! \\--
-    ['Owner'] = 'CleanedByNob0dy',
-    ['Position'] = 'Alt', --Position Of Stand [Back | Left | Right]
+    ['Owner'] = 'CleanedByxZyn',
+    ['Position'] = 'Right', --Position Of Stand [Back | Left | Right]
     ['CrewID'] = 32570691, --Must Be In Group For It To Work
     ['StandMode'] = 'Star Platinum : OverHeaven', --[Star Platinum : OverHeaven | Star Platinum: The World | Star Platinum, Za Warudo! | TheWorld | Cmoon | King Crimson | Killer Queen | MIH | D4C]
     ['FlyMode'] = 'Heaven', --[Glide | Fly | Hover | Heaven]
