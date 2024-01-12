58f7269fee6b295a8f28f427ee23e0a3fb0f95ea
Sky <85958646+SkyOnTop@users.noreply.github.com>
2024-01-12 00:51:42 +0000

    Update control.lua

--- control.lua
+++ control.lua
@@ -11,7 +11,7 @@ getgenv().LynixSettings = {
     ['FlyMode'] = 'Heaven', --[Glide | Fly | Hover | Heaven]
     ['Prediction'] = 0.34, --Prediction For Attack Or Targetting
     ['GunPrediction'] = 0.17, --Prediction For Gun Kill
-    ['AttackMode'] = 'Under', --[Sky | Under] If [Sky] The Stand Will Attack Infront Of Then And If [Under] The Stand Will Attack In Underground
+    ['AttackMode'] = 'Sky', --[Sky | Under] If [Sky] The Stand Will Attack Infront Of Then And If [Under] The Stand Will Attack In Underground
     ['Resolver'] = false, --[true | false] Turn On Or Off Resolver If Someone Using Anti Lock
     --// ! Misc ! \\--
     ['CustomSummon'] = 'Summon!', --Custom [Summon!] Text
@@ -20,7 +20,7 @@ getgenv().LynixSettings = {
     ['AntiStomp'] = true, --[true | false] The Stand Will Cannot Be Stomped Or Grabbed
     ['CustomName'] = 'Master', --Custom Name Text Default Is [Master] Example Of it 'Im Here, Master!'
     ['Hidescreen'] = true, --[true | false] If [true] You WIll See White Blank Screen But The Performance Is Gonna Be Good]
-    ['ChatCmds'] = true, --[true | false] Chatted When You Use Some Command
+    ['ChatCmds'] = false, --[true | false] Chatted When You Use Some Command
     ['AutoMask'] = true, --[true | false] Auto Buy Mask When The Stand Respawn
     ['MaskMode'] = 'Paintball', --[Surgeon | Paintball | Skull | Pumpkin | Hockey | Ninja | Riot] The Stand Will Buy The Mask You Selected
     ['FPSCAP'] = false, --[false | This is so your main account gets the most fps (MAXED IS 60, SUGGEST 30 FPS ON STANDS)
