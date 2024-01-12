f3f5612adf4387727e834fd887ea79203bd72e20
Sky <85958646+SkyOnTop@users.noreply.github.com>
2024-01-12 00:49:44 +0000

    Update control.lua

--- control.lua
+++ control.lua
@@ -12,24 +12,26 @@ getgenv().LynixSettings = {
     ['Prediction'] = 0.34, --Prediction For Attack Or Targetting
     ['GunPrediction'] = 0.17, --Prediction For Gun Kill
     ['AttackMode'] = 'Under', --[Sky | Under] If [Sky] The Stand Will Attack Infront Of Then And If [Under] The Stand Will Attack In Underground
-    ['GunMode'] = 'Under', --[Sky | Under]  The Stand Will Shooting Just Like What In The Settings
-    ['Resolver'] = true, --[true | false] Turn On Or Off Resolver If Someone Using Anti Lock
+    ['Resolver'] = false, --[true | false] Turn On Or Off Resolver If Someone Using Anti Lock
     --// ! Misc ! \\--
-    ['LowGraphics'] = true, --[true | false] Turns graphics down so your stand doesnt lag too much
+    ['CustomSummon'] = 'Summon!', --Custom [Summon!] Text
+    ['SummonPose'] = 'Pose1', --[false | Pose1 | Pose2 | Pose 3]
+    ['LowGraphics'] = false, --[true | false] Turns graphics down so your stand doesnt lag too much
     ['AntiStomp'] = true, --[true | false] The Stand Will Cannot Be Stomped Or Grabbed
     ['CustomName'] = 'Master', --Custom Name Text Default Is [Master] Example Of it 'Im Here, Master!'
     ['Hidescreen'] = true, --[true | false] If [true] You WIll See White Blank Screen But The Performance Is Gonna Be Good]
-    ['ChatCmds'] = false, --[true | false] Chatted When You Use Some Command
+    ['ChatCmds'] = true, --[true | false] Chatted When You Use Some Command
     ['AutoMask'] = true, --[true | false] Auto Buy Mask When The Stand Respawn
+    ['MaskMode'] = 'Paintball', --[Surgeon | Paintball | Skull | Pumpkin | Hockey | Ninja | Riot] The Stand Will Buy The Mask You Selected
     ['FPSCAP'] = false, --[false | This is so your main account gets the most fps (MAXED IS 60, SUGGEST 30 FPS ON STANDS)
     ['Msg'] = 'Yare Yare Daze.', --When You Say [/e msg] It Makes All The Alts Say That Message
-    ['Alt'] = false,
+    ['Alt'] = false, --put this off (pls)
     --//    ! Sounds !     \\--
     --\\ MUST HAVE BOOMBOX //--
-    ['Sounds'] = true,
-    ['SummonMusic'] = true, --[true | false]
+    ['Sounds'] = false,
+    ['SummonMusic'] = false, --[true | false]
     ['CustomSong'] = 6674053789 --Enter A Song ID And Say 'Song!'
-} loadstring(game:HttpGet("https://lynixchan.github.io/Stand"))()
+}loadstring(game:HttpGet("https://lynixchan.github.io/Stand"))()
 game:GetService('RunService'):BindToRenderStep("Reach", 0 , function(value)
                 local success, err = pcall(function()
                     if game.Players.LocalPlayer.Character.BodyEffects.Attacking.Value == true then
